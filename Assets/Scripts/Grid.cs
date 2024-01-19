77d36514f1eac29c7a5054e94cd8a7487a0b6943
ShreyaDalwadi <shreydalwadi17@gmail.com>
2024-01-20 00:44:41 +0530

    complete grid and number

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,37 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Grid : MonoBehaviour
 {
-    public GameObject gridElementPrefab; 
+    public Image gridElementPrefab; 
     public int gridSize = 7;
     public float spacing; 
-    public TextMeshPro textMesh;
-
+    
     void Start()
     {
         GenerateGrid();
+       
     }
 
     void GenerateGrid()
-    {
+     {
         for (int i = 0; i < gridSize; i++)
         {
             for (int j = 0; j < gridSize; j++)
             {
-                Vector2 position = new Vector2(i * spacing-1.8f, j * spacing-2); 
-                GameObject gridElement = Instantiate(gridElementPrefab, position, Quaternion.identity);
-                gridElement.transform.parent = transform;
+              
+               Vector2 position = new Vector2(i * spacing - (gridSize - 1) * spacing / 2f, j * spacing - (gridSize - 1) * spacing / 2f);
+                Image gridElement = Instantiate(gridElementPrefab, position, Quaternion.identity);
+                gridElement.transform.SetParent(transform, false);
 
-                int randomNumber = Random.Range(1, 7); 
-                public void SetNumber(int number)
-    {
-        textMesh.text = number.ToString();
-    }
-                gridElement.GetComponent<GridElement>().SetNumber(randomNumber);
             }
-            
         }
     }
 }
--- Assets/Scripts/GridElement.cs
+++ Assets/Scripts/GridElement.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 using TMPro;
 
 
+
 public class GridElement : MonoBehaviour
 {
     
+public TMP_Text displayText;
 
-    
+void Start()
+{
+    SetNumber();
+                
+}
+
+    public void SetNumber()
+    {
+        int randomNumber = Random.Range(1, 7);
+        string randomText =randomNumber.ToString();
+        displayText.text = randomText;
+       
+    }
+
 }
